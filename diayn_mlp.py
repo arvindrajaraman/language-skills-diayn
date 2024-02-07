47fd97e5e6e099ca91f2155b26b2919793be28b5
Arvind Rajaraman <arvind6902@gmail.com>
2024-02-07 13:52:54 -0800

    Updated hyperparams

--- diayn_mlp.py
+++ diayn_mlp.py
@@ -1,11 +1,9 @@
 import os
-# os.environ['PYOPENGL_PLATFORM'] = 'egl'
 
 from datetime import datetime
 import random
 
 import gym
-from gym.wrappers import RecordVideo
 import torch
 from tqdm import tqdm
 import wandb
@@ -14,46 +12,46 @@ wandb.login()
 from dqn_diayn import Agent
 
 config = {
+    "action_size": 4,
+    "batch_size": 128,
+    "buffer_size": 10000,
+    "discrim_lr": 0.01,
+    "discrim_momentum": 0.99,
     "env_name": "LunarLander-v2",
     "episodes": 5000,
+    "eps_decay": 0.9995,
+    "eps_end": 0.01,
+    "eps_start": 1.0,
+    "gamma": 0.7,
     "max_steps_per_episode": 300,
-    "state_size": 8,
-    "action_size": 4,
-    "skill_size": 5,
-    "buffer_size": 10000,
-    "batch_size": 512,
+    "policy_lr": 1e-5,
     "seed": 42,
-    "tau": 1e-3,                # for soft update of target parameters
-    "update_every": 1,          # how often to update the network
-    "discrim_momentum": 0.99,
-
-    "eps_start": 0.01,
-    "eps_end": 0.25,
-    "eps_decay": 0.9,
-    "discrim_lr": 0.01,
-    "policy_lr": 1e-5,          # learning rate
-    "gamma": 1.0,              # discount factor
+    "skill_size": 5,
+    "state_size": 8,
+    "tau": 0.0001,       # for soft update of target parameters
+    "update_every": 1,
 }
 
 
 def generate_test_video(agent, skill_idx, iter):
-    test_env = gym.make(config["env_name"])
-    test_env.seed(config["seed"] + 1)
-    # test_env = RecordVideo(test_env, f'./data/{run_name}/iter{iter}_skill{skill_idx}')
+    # test_env = gym.make(config["env_name"])
+    # test_env.seed(config["seed"] + 1)
+    # # test_env = RecordVideo(test_env, f'./data/{run_name}/iter{iter}_skill{skill_idx}')
 
-    obs = test_env.reset()
-    for _ in range(config["max_steps_per_episode"]):
-        action = agent.act(obs, skill_idx, eps=0.)
-        # test_env.capture_frame()
-        next_obs, _, done, _ = test_env.step(action)
-        if done:
-            break
+    # obs = test_env.reset()
+    # for _ in range(config["max_steps_per_episode"]):
+    #     action = agent.act(obs, skill_idx, eps=0.)
+    #     # test_env.capture_frame()
+    #     next_obs, _, done, _ = test_env.step(action)
+    #     if done:
+    #         break
 
-        test_env.render(mode="rgb_array")
+    #     test_env.render(mode="rgb_array")
 
-        obs = next_obs
+    #     obs = next_obs
 
-    test_env.close()
+    # test_env.close()
+    pass
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 run_name = '{}_{}'.format(config["env_name"], int(datetime.now().timestamp()))
@@ -62,7 +60,8 @@ os.makedirs(f'./data/{run_name}')
 # Initialize wandb
 run = wandb.init(
     project="language-skills",
-    entity="arvind6902"
+    entity="arvind6902",
+    name=run_name,
 )
 wandb.config = config
 
@@ -74,12 +73,7 @@ env.action_space.seed(config["seed"])
 agent = Agent(config=config, conv=False)
 
 eps = config["eps_start"]
-for episode in tqdm(range(config["episodes"])):
-    if episode % 500 == 0:
-        print('Saving videos...')
-        for i in tqdm(range(config["skill_size"])):
-            generate_test_video(agent, i, episode)
-    
+for episode in tqdm(range(config["episodes"])):  
     # Sample skill and initial state
     skill_idx = random.randint(0, config["skill_size"] - 1)
     obs = env.reset()
@@ -98,7 +92,7 @@ for episode in tqdm(range(config["episodes"])):
             break
 
     eps = max(eps * config["eps_decay"], config["eps_end"])
-    if episode % 100 == 0:
-        torch.save(agent.qnetwork_local.state_dict(), f'./data/{run_name}/qnetwork_local.pth')
-        torch.save(agent.qnetwork_target.state_dict(), f'./data/{run_name}/qnetwork_target.pth')
-        torch.save(agent.discriminator.state_dict(), f'./data/{run_name}/discriminator.pth')
+    if episode % 500 == 0:
+        torch.save(agent.qnetwork_local.state_dict(), f'./data/{run_name}/qnetwork_local_iter{episode}.pth')
+        torch.save(agent.qnetwork_target.state_dict(), f'./data/{run_name}/qnetwork_target_iter{episode}.pth')
+        torch.save(agent.discriminator.state_dict(), f'./data/{run_name}/discriminator_iter{episode}.pth')
