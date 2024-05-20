7cf79be9b7bf57885791909b343577d1d09faf83
Arvind Rajaraman <arvind6902@gmail.com>
2024-05-20 11:45:13 -0700

    updates

--- slurm/command.sh
+++ slurm/command.sh
@@ -4,9 +4,9 @@
 #SBATCH --gpus=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=8gb
-#SBATCH --qos=medium
-#SBATCH --output=slurm/1716228096-out.txt
-#SBATCH --error=slurm/1716228096-err.txt
+#SBATCH --qos=scavenger
+#SBATCH --output=slurm/1716230592-out.txt
+#SBATCH --error=slurm/1716230592-err.txt
 
 TOTAL_MEMORY=49140
 TARGET_MEMORY=3686
@@ -14,4 +14,4 @@ FRACTION=0.07501017501017501
 echo "Allocating $FRACTION of GPU ($TARGET_MEMORY MiB out of $TOTAL_MEMORY MiB)"
 export XLA_PYTHON_CLIENT_MEM_FRACTION=$FRACTION
 
-srun conda run --no-capture-output -n diayn4 python diayn_fast.py -c paper/diayn_lang.yml
+srun conda run --no-capture-output -n diayn4 python diayn_fast.py -c paper/diayn_lang_hybrid.yml
